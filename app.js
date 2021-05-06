fa91c2bb980e6d6a3770c0e4d2ed9d494bc5451b
Andy Davies <andy.davies@drdoctor.co.uk>
2021-05-06 21:43:21 +0100

    Fix eslint suggestions / errors.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var express = require('express');
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
 
-var app = express();
+const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    res.send('hello from test');
+app.get('/', (req, res) => {
+  res.send('hello from test');
 });
 
 app.listen(port, () => {
-    debug(`Listening at port ${chalk.green(port)}`);
+  debug(`Listening at port ${chalk.green(port)}`);
 });
