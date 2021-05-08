ca101568e284dc713c7226367e674e2eba8aa931
Andy Davies <andy.davies@drdoctor.co.uk>
2021-05-08 15:01:21 +0100

    add ejs viewengine

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const debug = require('debug')('app');
 const chalk = require('chalk');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('hello from test');
+  res.render('index');
 });
 
 app.listen(port, () => {
